773c227870ca65c7fb597db4cce429be6d46c7f5
Andriy Redko <andriy.redko@aiven.io>
2024-11-14 15:06:02 -0500

    Support SecureSM injection
    Signed-off-by: Andriy Redko <andriy.redko@aiven.io>

--- src/main/java/org/opensearch/javaagent/Agent.java
+++ src/main/java/org/opensearch/javaagent/Agent.java
@@ -1,35 +1,35 @@
 package org.opensearch.javaagent;
 
+import java.io.File;
 import java.lang.instrument.Instrumentation;
 import java.nio.channels.SocketChannel;
+import java.nio.file.Files;
+import java.util.Map;
+
+import org.opensearch.secure_sm.SecureSM;
+import org.opensearch.secure_sm.ThreadPermission;
 
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.ClassFileLocator;
+import net.bytebuddy.dynamic.loading.ClassInjector;
 import net.bytebuddy.implementation.Implementation;
 import net.bytebuddy.matcher.ElementMatcher.Junction;
 import net.bytebuddy.matcher.ElementMatchers;
 
 public class Agent {
-	private static boolean loadedDynamically = false;
 
-	public static void premain(String agentArguments, Instrumentation instrumentation) {
-		System.out.print("Starting from premain");
-
-		AgentBuilder agentBuilder = createAgentBuilder(instrumentation);
-		agentBuilder.installOn(instrumentation);
+	public static void premain(String agentArguments, Instrumentation instrumentation) throws Exception {
+		initAgent(instrumentation);
 	}
 
-	public static void agentmain(String agentArguments, Instrumentation instrumentation) {
-		System.out.print("Starting from agentmain");
-		loadedDynamically = true;
-
-		AgentBuilder agentBuilder = createAgentBuilder(instrumentation);
-		agentBuilder.installOn(instrumentation);
+	public static void agentmain(String agentArguments, Instrumentation instrumentation) throws Exception {
+		initAgent(instrumentation);
 	}
 
-	private static AgentBuilder createAgentBuilder(Instrumentation inst) {
+	private static AgentBuilder createAgentBuilder(Instrumentation inst) throws Exception {
 	     final Junction<TypeDescription> systemType = ElementMatchers.isSubTypeOf(SocketChannel.class);
 
 	     final AgentBuilder.Transformer transformer =
@@ -37,7 +37,11 @@ public class Agent {
 	                .visit(Advice.to(SocketChannelInterceptor.class).on(ElementMatchers.named("connect")
 	                    .and(ElementMatchers.not(ElementMatchers.isAbstract()))));
 
-	     // Disable a bunch of stuff and turn on redefine as the only option
+          final File temp = Files.createTempDirectory("tmp").toFile();
+          ClassInjector.UsingInstrumentation.of(temp, ClassInjector.UsingInstrumentation.Target.BOOTSTRAP, inst).inject(Map.of(
+              new TypeDescription.ForLoadedType(SecureSM.class), ClassFileLocator.ForClassLoader.read(SecureSM.class),
+              new TypeDescription.ForLoadedType(ThreadPermission.class), ClassFileLocator.ForClassLoader.read(ThreadPermission.class)));
+
 	     final ByteBuddy byteBuddy = new ByteBuddy().with(Implementation.Context.Disabled.Factory.INSTANCE);
 	     final AgentBuilder agentBuilder = new AgentBuilder.Default(byteBuddy)
              .with(AgentBuilder.InitializationStrategy.NoOp.INSTANCE)
@@ -48,5 +52,10 @@ public class Agent {
              .transform(transformer);
 
 	     return agentBuilder;
+	}
+	
+	private static void initAgent(Instrumentation instrumentation) throws Exception {
+		AgentBuilder agentBuilder = createAgentBuilder(instrumentation);
+		agentBuilder.installOn(instrumentation);
 	}
 }
--- src/main/java/org/opensearch/javaagent/SocketChannelInterceptor.java
+++ src/main/java/org/opensearch/javaagent/SocketChannelInterceptor.java
@@ -2,7 +2,8 @@ package org.opensearch.javaagent;
 
 import java.lang.reflect.Method;
 import java.net.SocketPermission;
-import java.security.AccessController;
+
+import org.opensearch.secure_sm.SecureSM;
 
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.asm.Advice.Origin;
@@ -10,7 +11,7 @@ import net.bytebuddy.asm.Advice.Origin;
 public class SocketChannelInterceptor {
 	@Advice.OnMethodEnter
 	public static void intercept(@Origin Method method) throws Exception {
-		AccessController.checkPermission(new SocketPermission("localhost", "connect"));
-		throw new SecurityException();
+		final SecureSM sm = new SecureSM();
+		sm.checkPermission(new SocketPermission("localhost", "connect"));
 	}
 }
